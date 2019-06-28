1d86387db9a5569167cde9d4845f3e4144e02d6b
Alexander <samik.mechanic@gmail.com>
2019-06-28 11:24:54 +0300

    правка клавиатур
    * клавиатура боя содержит кнопки старта/стопа (только офы)
    * клавиатура начавшегося боя содержит кнопки отмены и стопа

--- keyboards.py
+++ keyboards.py
@@ -8,20 +8,30 @@
 from telebot import types
 
 # keyboards
-# check
+# check (from the battle chat)
 KEYBOARD_CHECK = types.InlineKeyboardMarkup(row_width=3)
 buttonPlus = types.InlineKeyboardButton(text="✅", callback_data="✅")
 buttonRage = types.InlineKeyboardButton(text="🔥", callback_data="🔥")
 buttonArsenal = types.InlineKeyboardButton(text="📦", callback_data="📦")
-buttonCancel = types.InlineKeyboardButton(text="❌", callback_data="❌")
 buttonThinking = types.InlineKeyboardButton(text="💤", callback_data="💤")
-KEYBOARD_CHECK.add(buttonPlus, buttonRage, buttonArsenal, buttonThinking, buttonCancel)
+buttonSpacer = types.InlineKeyboardButton(text=" ", callback_data="spacer")
+buttonCancel = types.InlineKeyboardButton(text="❌", callback_data="❌")
+buttonStart = types.InlineKeyboardButton(text="▶️", callback_data="▶️")
+buttonStop = types.InlineKeyboardButton(text="⏹", callback_data="⏹")
+KEYBOARD_CHECK.add(buttonPlus, buttonRage, buttonArsenal, 
+                   buttonThinking, buttonSpacer, buttonCancel,
+                   buttonStart, buttonStop)
 
-KEYBOARD_LATE = types.InlineKeyboardMarkup(row_width=1)
+# for started battle
+KEYBOARD_LATE = types.InlineKeyboardMarkup(row_width=2)
 buttonLate = types.InlineKeyboardButton(text="⏰", callback_data="⏰")
-KEYBOARD_LATE.add(buttonLate)
+KEYBOARD_LATE.add(buttonCancel, buttonLate, buttonStop)
 
-# battle control
+CHECK_OPTIONS = [buttonPlus.text, buttonRage.text, buttonArsenal.text,
+                 buttonCancel.text, buttonThinking.text, buttonLate.text]
+CONTROL_OPTIONS = [buttonStart.text, buttonStop.text]
+
+# battle control (from the bot chat)
 KEYBOARD_START = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
 KEYBOARD_STOP = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
 buttonStart = types.KeyboardButton(text="▶️ Запустить")
