e7aa1ca38729e324775e4353d1952d095a8c88c9
Alexander <samik.mechanic@gmail.com>
2020-10-26 18:37:38 +0300

    правка 0941204e339faae86fd7abc74e8cd5bba568cb6c

--- bot.py
+++ bot.py
@@ -658,7 +658,7 @@ def debug_test_command(m):
         log.debug("Debug mode is ON, processing...")
         user = User(m.from_user.id, m.from_user.first_name, m.from_user.username)
         text = "User %s sent /test command." % user
-        temp_file = tempfile.NamedTemporaryFile(mode="w", delete=False, suffix='.txt', prefix=user.username+"_")
+        temp_file = tempfile.NamedTemporaryFile(mode="w", delete=False, suffix='.txt')
         # write whole message object into file (change on needs)
         temp_file.write(str(m))
         temp_file.close()
